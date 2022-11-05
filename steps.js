953d4f426a57997f9222cffa87be2749e35789c8
HIRATOQEER <86204569+HIRATOQEER@users.noreply.github.com>
2022-11-05 07:28:11 -0700

    Add files via upload

---
+++ steps.js
@@ -0,0 +1,30 @@
+const { I,And } = inject();
+// Add in your custom step files
+
+Given('I am on the subcategory1', () => {
+  // TODO: replace with your own step
+  
+  I.amOnPage('https://demo.reactstorefront.io/s/1?color=red&size=sm&type=new&sort=price_asc')
+  I.seeInTitle('Subcategory 1');
+});
+
+When('I Select the option colour,size and type And click clear all',()=>{
+  I.seeElement('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[1]/div[2]/div/div/div/div/div/div[1]/button')
+  I.click('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[1]/div[2]/div/div/div/div/div/div[1]/button')
+  I.seeElement('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div/button[1]')
+  I.click('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div/button[1]')
+  I.seeElement('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[3]/div[2]/div/div/div/div/div/label[1]/span[1]/span[1]/input')
+  I.click('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[3]/div[2]/div/div/div/div/div/label[1]/span[1]/span[1]/input')
+  I.seeElement('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[1]/button')
+  I.click('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[1]/button')
+});
+
+Then('All the selected items should be cleared',()=>{
+  I.amOnPage('https://demo.reactstorefront.io/s/1?sort=price_asc')
+  I.seeElement('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[1]/div[2]/div/div/div/div/div/div[1]/button')
+ //I.click('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[1]/div[2]/div/div/div/div/div/div[1]/button')
+ I.seeElement('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div/button[1]')
+ //I.click('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div/button[1]')
+ I.seeElement('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[3]/div[2]/div/div/div/div/div/label[1]/span[1]/span[1]/input')
+ //I.click('//*[@id="__next"]/main/div/div[2]/div[1]/div/div/div[2]/div[3]/div[2]/div/div/div/div/div/label[1]/span[1]/span[1]/input')
+});
