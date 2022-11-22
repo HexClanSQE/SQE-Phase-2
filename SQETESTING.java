de1cd5bb3d290c06cb14a51b3c8a14c25cd83505
HIRATOQEER <86204569+HIRATOQEER@users.noreply.github.com>
2022-11-22 01:04:26 -0800

    Add files via upload

---
+++ SQETESTING.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sqetesting;
+
+/**
+ *
+ * @author l2013
+ */
+public class SQETESTING {
+public static int findMax(int arr[]){  
+        int max=0;  
+        for(int i=1;i<arr.length;i++){  
+            if(max<arr[i])  
+                max=arr[i];  
+        }  
+        return max;  
+    }  
+    
+}
